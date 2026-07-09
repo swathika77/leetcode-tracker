b5d1e7322f35be38671413eb33ab66df17d00ec1
SWATHIKA K <swathikasi022@gmail.com>
2026-07-09 10:09:38 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 10:10:09
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int[] filteredNums = Arrays.stream(nums).filter(n -> n > 0).toArray();
+        
+        Arrays.sort(filteredNums);
+        
+        int target = 1;
+        for (int n : filteredNums) {
+            if (n == target) {
+                target++;
+            } else if (n > target) {
+                return target;
+            }
+        }
+        
+        return target;        
+    }
+}
