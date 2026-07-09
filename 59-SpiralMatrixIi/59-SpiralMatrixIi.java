469f62aad66f2532a1cf3deee787da9813565c2d
SWATHIKA K <swathikasi022@gmail.com>
2026-07-09 10:09:07 +0530

    Create 59-SpiralMatrixIi

---
+++ 59-SpiralMatrixIi/59-SpiralMatrixIi.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 10:09:39
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int x = 0, y = 0, dx = 1, dy = 0;
+        int[][] res = new int[n][n];
+
+        for (int i = 0; i < n * n; i++) {
+            res[y][x] = i + 1;
+
+            if (!(0 <= x + dx && x + dx < n && 0 <= y + dy && y + dy < n && res[y+dy][x+dx] == 0)) {
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+            }
+            
+            x += dx;
+            y += dy;
+        }
+
+        return res;        
+    }
+}
