443a42eec168c66481c4678d736bea5924eb85e5
SWATHIKA K <swathikasi022@gmail.com>
2026-07-09 10:07:33 +0530

    Create 99-RecoverBinarySearchTree

---
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -0,0 +1,24 @@
+// Last updated: 09/07/2026, 10:08:04
+class Solution {
+    TreeNode prev=null,first=null,second=null;
+    void inorder(TreeNode root){
+        if(root==null)
+            return ;
+        inorder(root.left);
+        if(prev!=null&&root.val<prev.val){
+            if(first==null)
+                first=prev;
+            second=root;
+        }
+        prev=root;
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        if(root==null)
+            return ; 
+        inorder(root);
+        int temp=first.val;
+        first.val=second.val;
+        second.val=temp;
+    }
+}
