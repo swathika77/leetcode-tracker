5b159a9fb4009880611fc1df22592e680d1826a9
SWATHIKA K <swathikasi022@gmail.com>
2026-07-09 10:10:13 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,50 @@
+// Last updated: 09/07/2026, 10:10:44
+class Solution {
+    public boolean isValid(String s) {
+
+
+        // Create a new stack to store the characters.
+        Stack<Character> stack = new Stack<>();
+
+
+        // convert string into char array and access the characters using for each loop.
+        for(char ch: s.toCharArray())
+        {
+            // check ch
+            switch (ch)
+            {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                        // hence it is invalid.
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return stack.isEmpty();
+        
+    }
+}
