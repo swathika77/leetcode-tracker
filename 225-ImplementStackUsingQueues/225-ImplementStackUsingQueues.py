7a8a9d3b6f4bd2cd44feadcdb68357783adcd42a
SWATHIKA K <swathikasi022@gmail.com>
2026-07-15 15:11:29 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.py
@@ -0,0 +1,21 @@
+# Last updated: 15/07/2026, 15:12:01
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        v = len(self.q) - 1
+        i = 0
+        while i < v:
+            self.q.append(self.q.popleft())
+            i += 1
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
