cd573eac292299177d5c4aca62e9ee83bb18df28
SWATHIKA K <swathikasi022@gmail.com>
2026-07-15 15:11:30 +0530

    Create 224-BasicCalculator

---
+++ 224-BasicCalculator/224-BasicCalculator.py
@@ -0,0 +1,21 @@
+# Last updated: 15/07/2026, 15:12:03
+class Solution:
+  def calculate(self, s: str) -> int:
+    ans = 0
+    num = 0
+    sign = 1
+    stack = [sign]  # stack[-1]: current env's sign
+
+    for c in s:
+      if c.isdigit():
+        num = num * 10 + (ord(c) - ord('0'))
+      elif c == '(':
+        stack.append(sign)
+      elif c == ')':
+        stack.pop()
+      elif c == '+' or c == '-':
+        ans += sign * num
+        sign = (1 if c == '+' else -1) * stack[-1]
+        num = 0
+
+    return ans + sign * num
