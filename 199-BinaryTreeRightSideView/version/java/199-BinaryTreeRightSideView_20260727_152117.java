2045024567be598905b6b81310dcdf271e4069f3
SWATHIKA K <swathikasi022@gmail.com>
2026-07-27 15:20:46 +0530

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/version/java/199-BinaryTreeRightSideView_20260727_152117.java
@@ -0,0 +1,16 @@
+// Last updated: 27/07/2026, 15:21:17
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode ans = new ListNode(0, head);
+        ListNode dummy = ans;
+
+        while (dummy != null) {
+            while (dummy.next != null && dummy.next.val == val) {
+                dummy.next = dummy.next.next;
+            }
+            dummy = dummy.next;
+        }
+
+        return ans.next;        
+    }
+}
